7052f8ae380a8821a03ab205be0968f7bde2f014
symarys <symarys@gmail.com>
2015-06-16 12:24:04 +0200

    Initializing new plugin

---
+++ init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :user_expiration do
+  name 'User Expiration plugin'
+  author 'Maria Syczewska'
+  description 'This is a plugin for Redmine for implementing and managing user expiration date'
+  version '0.0.1'
+end
